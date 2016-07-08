3b5b937c172779a968c0d29dc41c8b58fa6b2f89
= <ehsan5505@gmail.com>
2016-07-09 04:17:21 +0500

    adding error page

--- thermos.py
+++ thermos.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html')
+
+@app.errorhandler(500)
+def server_error(e):
+	return render_template('500.html')
+
 @app.route('/add')
 def add():
 	return render_template('add.html')
