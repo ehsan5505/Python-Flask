cfa47230434636850f6c7d35c40dfa5eb86e62a4
= <ehsan5505@gmail.com>
2016-07-09 05:29:47 +0500

    /add post request log the url

--- thermos.py
+++ thermos.py
@@ -1,6 +1,18 @@
-from flask import Flask,url_for,render_template
+from flask import Flask,url_for,render_template,request
+from logging import DEBUG
+from datetime import datetime
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
+
+bookmarks = []
+
+def store_bookmark(url):
+	bookmarks.append(dict(
+		url = url,
+		name = 'erafeeque',
+		datetime = datetime.utcnow() 
+	))
 
 @app.route('/')
 @app.route('/index')
@@ -15,9 +27,14 @@ def page_not_found(e):
 def server_error(e):
 	return render_template('500.html')
 
-@app.route('/add')
+@app.route('/add',methods=['GET','POST'])
 def add():
+	if request.method == 'POST':
+		url = request.form['url']
+		store_bookmark(url)
+		app.logger.debug('Store URL: '+url)
 	return render_template('add.html')
+		# app.logger.debug('Store URL : '+ url )
 
 # run the main applcation
 if __name__ == '__main__':
